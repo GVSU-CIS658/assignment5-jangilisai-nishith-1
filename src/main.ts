547a304f061b1fd87ddcbb1b09b200a9993a8463
jangilisai-nishith <jangilis@mail.gvsu.edu>
2026-04-17 14:00:32 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import "./styles/mug.scss";
 import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import App from "./App.vue";
+
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
 
 createApp(App).use(pinia).mount("#app");
+
+import { useBeverageStore } from "./stores/beverageStore";
+const beverageStore = useBeverageStore();
+beverageStore.init();
