36918186e6fc59eb0a0723fcd708d981a9cdde02
jangilisai-nishith <jangilis@mail.gvsu.edu>
2026-04-17 18:06:23 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,7 @@ import "./styles/mug.scss";
 import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import App from "./App.vue";
-import { useBeverageStore } from "./stores/beverageStore";
-
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
 
-const app = createApp(App);
-app.use(pinia);
-
-async function init() {
-  const store = useBeverageStore();
-  await store.init();
-  app.mount("#app");
-}
-
-init();
+createApp(App).use(pinia).mount("#app");
